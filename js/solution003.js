9c4bb9d4c699d0a09dd49fd22f7d25248dd1cd0d
donovanlnguyen <donovanlnguyen@gmail.com>
2022-05-28 12:36:59 -0700

    solution003 completed

---
+++ js/solution003.js
@@ -0,0 +1,8 @@
+//Write a function which calculates the average of the numbers in a given list.
+
+function find_average(arr) {
+    return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
+}
+
+//const find_average =(arr) => !arr || arr.length === 0 ? 0
+// : arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
