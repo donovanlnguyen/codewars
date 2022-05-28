cc002b2b20aa3db3c2841e19c48e5bd361fb8898
donovanlnguyen <donovanlnguyen@gmail.com>
2022-05-28 12:58:29 -0700

    solution004 completed

---
+++ js/solution004.js
@@ -0,0 +1,11 @@
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+function invert(array) {
+    return array.map(num => {
+        return num *-1
+      });
+ }
+
+ //function invert(array) {
+ //   return array.map(num => num * -1)
+//}
