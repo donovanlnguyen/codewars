31734c3f991dd7bb95b0311eb2f67483d4856788
donovanlnguyen <donovanlnguyen@gmail.com>
2022-06-04 12:26:46 -0700

    solution016 completed

---
+++ js/solution016.js
@@ -0,0 +1,7 @@
+//The first century spans from the year 1 up to and including the year 100, the second century - 
+//from the year 101 up to and including the year 200, etc.
+//Given a year, return the century it is in.
+
+function century(year) {
+    return Math.ceil(year / 100)
+  }
