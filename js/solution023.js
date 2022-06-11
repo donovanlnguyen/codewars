0c1d7c5bc6e0a94f81d57367aeaa7a1b53ab0d2f
donovanlnguyen <donovanlnguyen@gmail.com>
2022-06-11 11:11:37 -0700

    solution023 completed

---
+++ js/solution023.js
@@ -0,0 +1,5 @@
+//Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+//Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+const binaryArrayToNumber = arr => {
+  return parseInt(arr.join(""), 2)
+};
