505ada72071898b19baefa1d47618154a5531c76
donovanlnguyen <donovanlnguyen@gmail.com>
2022-06-20 13:19:59 -0700

    solution033 completed

---
+++ js/solution033.js
@@ -0,0 +1,13 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+function solution(string) {
+    let newString = ''
+    for(letter of string) {
+      if(letter == letter.toUpperCase()){
+        newString += ' '
+        newString += letter
+      }else{
+        newString += letter
+      }
+    }
+    return newString
+  }
