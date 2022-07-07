f19bf6a27cf86c8f3fa1e0b3d18bc4d5dd754715
donovanlnguyen <donovanlnguyen@gmail.com>
2022-07-07 10:36:43 -0700

    solution051 completed

---
+++ js/solution051.js
@@ -0,0 +1,7 @@
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+function nearestSq(n){
+    let nearSq = Math.pow(Math.round(Math.sqrt(n)), 2)
+     return nearSq
+   }
+
+//const nearestSq = n => Math.pow(Math.round(Math.sqrt(n)), 2);
