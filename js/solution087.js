1dba07d75fd6fa62160b3426853cd3f85d97e6a5
donovanlnguyen <donovanlnguyen@gmail.com>
2022-08-10 13:13:31 -0700

    solution087 completed

---
+++ js/solution087.js
@@ -0,0 +1,6 @@
+// Write a simple regex to validate a username. Allowed characters are:
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+validateUsr = username => /^[a-z\d_]{4,16}$/.test(username)
