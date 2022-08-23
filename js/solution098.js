ba06864b003e62389facfa883b622e447befb73b
donovanlnguyen <donovanlnguyen@gmail.com>
2022-08-22 19:20:46 -0700

    solution098 completed

--- js/solution098.js
+++ js/solution098.js
@@ -1,3 +1,9 @@
 // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 // Hint for R users:
 // The length of string is not always the same as the number of characters
+function solution(a, b){
+    if(a.length > b.length){
+      return b.concat(a,b)
+    }else
+      return a.concat(b,a)
+  }
