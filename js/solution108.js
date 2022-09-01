0c60aa30a758d3e6c31e718b35b68bfbb825e1ca
donovanlnguyen <donovanlnguyen@gmail.com>
2022-09-01 11:45:40 -0700

    solution108 completed

---
+++ js/solution108.js
@@ -0,0 +1,4 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+function combineNames(first, last){
+    return first.concat(" ",last)
+  }
