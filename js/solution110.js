a2c12353d4d0e084f330914743a33ad2b4baf0ef
donovanlnguyen <donovanlnguyen@gmail.com>
2022-09-04 13:53:59 -0700

    solution111 completed

--- js/solution110.js
+++ js/solution110.js
@@ -2,3 +2,8 @@
 // Please return the gap between the first position of c and the last position of c.
 // If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
 const firstToLast = (str, c) => str.indexOf(c) < 0 ? -1 : str.lastIndexOf(c) - str.indexOf(c)
+
+// function firstToLast(str,c){
+//     var first = str.indexOf(c), last = str.lastIndexOf(c);
+//     return first == -1 ? -1 : last - first;
+//   }
---
+++ js/solution111.js
@@ -0,0 +1,5 @@
+// Write a function that will accept two parameters: 
+// variable and type and check if type of variable is matching type. Return true if types match or false if not.
+function typeValidation(variable, type) {
+    return typeof variable === type
+  }
