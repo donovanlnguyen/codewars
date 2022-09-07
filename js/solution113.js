9527ff1b02440b2bee878130013dc9b1c458a2bf
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-09-07 15:21:50 -0700

    solution113 completed

--- js/solution113.js
+++ js/solution113.js
@@ -1,5 +1,11 @@
 // We want an array, but not just any old array, an array with contents!
-
 // Write a function that produces an array with the numbers 0 to N-1 in it.
-
-// For example, the following code will result in an array containing the numbers 0 to 4:
+// For example, the following code will result in an array containing the numbers 
+// 0 to 4:
+const arr = N => {
+    const arr = []
+    for (let i = 0; i < N; i+=1){
+      arr.push(i)
+    }
+    return arr;
+  }
