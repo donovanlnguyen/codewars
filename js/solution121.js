83e6597e69ac7b5083ac09b195c61035980f0058
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-09-16 11:51:35 -0700

    solution121 completed

--- js/solution121.js
+++ js/solution121.js
@@ -1,4 +1,6 @@
 // Find the mean (average) of a list of numbers in an array.
 var findAverage = function (nums) {
-    // Code here
+    let total = nums.reduce((a, b) => a + b)
+    let average = total/nums.length
+    return average
   }
