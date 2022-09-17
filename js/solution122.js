4f825627c6498ab9cef7bc7de19dd243069f11f4
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-09-17 14:53:37 -0700

    solution122 added

---
+++ js/solution122.js
@@ -0,0 +1,8 @@
+// The main problem is your shyness: you're afraid that you'll end up blocking the view 
+// (even if only for a couple of seconds) of all the people who sit behind you and in your 
+// column or the columns to your left. To gain some courage, you decide to calculate the number
+// of such people and see if you can possibly make it to the exit without disturbing too many people.
+
+// Given the total number of rows and columns in the theater (nRows and nCols, respectively), 
+// and the row and column you're sitting in, return the number of people who sit strictly 
+// behind you and in your column or to the left, assuming all seats are occupied.
