6c5f6042577943d47d3154f607f2104ff7317dd3
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-09-18 18:52:06 -0700

    solution122 completed

--- js/solution122.js
+++ js/solution122.js
@@ -6,3 +6,6 @@
 // Given the total number of rows and columns in the theater (nRows and nCols, respectively), 
 // and the row and column you're sitting in, return the number of people who sit strictly 
 // behind you and in your column or to the left, assuming all seats are occupied.
+function seatsInTheater(nCols, nRows, col, row) {
+    return (nCols - col + 1) *(nRows - row)
+  }
