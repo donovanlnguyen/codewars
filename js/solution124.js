0d9866540b8559d4cd192e4ec519b0f0422f000e
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-09-21 15:36:09 -0700

    solution125 completed

--- js/solution124.js
+++ js/solution124.js
@@ -11,10 +11,8 @@
 // The numbers will also all be unique and in ascending order. The numbers could be 
 // positive or negative and the first non-consecutive could be either too!
 function firstNonConsecutive (arr) {
-    for (let i = 0; i < arr.length; i++) {
-    if (arr[i + 1] - arr[i] !== 1) {
-      return arr[i + 1];
-    }
-  }
-  return null;
-}
+    for (let i = 1; i < arr.length; i++) {
+           if (arr[i - 1] + 1 !== arr[i]) return arr[i];
+       }
+       return null;
+   }
