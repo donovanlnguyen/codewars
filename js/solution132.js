1627457d8e22d2a26c54240c77e2473293dd2f51
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-09-29 13:48:18 -0700

    solution132 completed

---
+++ js/solution132.js
@@ -0,0 +1,5 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+function reverseWords(str){
+    return str.split(" ").reverse().join(" "); // reverse those words
+  }
