d443405bb74298bd4ef69221cfc7534bd157cadf
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-10-07 13:28:20 -0700

    solution 138 continued

--- js/solution138.js
+++ js/solution138.js
@@ -1,1 +1,24 @@
 // The function is not returning the correct values. Can you figure out why?
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+      case 2:
+        name = 'Venus'
+      case 3:
+        name = 'Earth'
+      case 4:
+        name = 'Mars'
+      case 5:
+        name = 'Jupiter'
+      case 6:
+        name = 'Saturn'
+      case 7:
+        name = 'Uranus'
+      case 8:
+        name = 'Neptune'
+    }
+    
+    return name;
+  }
