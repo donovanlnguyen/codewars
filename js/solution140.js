7b2f04203e95388c4652453c99e2fdbb6bc78f01
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-10-12 13:50:53 -0700

    solution140 added

---
+++ js/solution140.js
@@ -0,0 +1,3 @@
+// Create a function that accepts 2 string arguments and returns an integer of the 
+// count of occurrences the 2nd argument is found in the first one.
+// If no occurrences can be found, a count of 0 should be returned.
