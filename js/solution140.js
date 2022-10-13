1be1f59dc030547df782aefbd595e73afc7ebaf5
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-10-13 13:57:00 -0700

    solution140 completed

--- js/solution140.js
+++ js/solution140.js
@@ -1,3 +1,6 @@
 // Create a function that accepts 2 string arguments and returns an integer of the 
 // count of occurrences the 2nd argument is found in the first one.
 // If no occurrences can be found, a count of 0 should be returned.
+function strCount(str, letter){  
+    return str.split(letter).length -1
+  }
