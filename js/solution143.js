0034700e7e9fa8a500d5ed851ccb1967a8886945
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-10-21 16:15:15 -0700

    solution143 completed

--- js/solution143.js
+++ js/solution143.js
@@ -1,2 +1,10 @@
 // The goal is to create a function 'numberToPower(number, power)' that "raises" 
 // the number up to power (ie multiplies number by itself power times).
+function numberToPower(number, power){
+    console.info(Math.log2(1024));
+    let result = 1
+    for(i = 1; i <= power; i++){
+      result = result * number;
+    }
+    return result
+  }
