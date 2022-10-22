53bf989bdc3788d0f0100bdcea3917007514b6ef
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-10-22 16:21:16 -0700

    solution144 added

---
+++ js/solution144.js
@@ -0,0 +1,5 @@
+// Write a function to get the first element(s) of a sequence. 
+// Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+
+// If n == 0 return an empty sequence []
