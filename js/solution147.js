40b64fc962fe1a0199857497d8556604cd6f2db8
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-10-30 13:48:00 -0700

    solution147 alt solution

--- js/solution147.js
+++ js/solution147.js
@@ -2,3 +2,9 @@
 function shortcut (string) {
     return string.replace(/[aeiou]/gi, '');
   }
+
+//   function shortcut(str) {
+//     return str.split('').filter(function(e) {
+//       return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+//     }).join('')
+//   }
