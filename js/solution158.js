d2f589cadf8a475cbad5234308e91c438cd28b9e
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-11-15 13:44:44 -0800

    solution158 completed

---
+++ js/solution158.js
@@ -0,0 +1,7 @@
+// Write a function that takes a list of strings as an argument and 
+// returns a filtered list containing the same elements but with the 'geese' removed.
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(x => !geese.includes(x))
+  };
