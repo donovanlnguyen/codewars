cb190fbc2b3fefd9e28dddcd8b6c42963dd20916
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-11-29 14:32:56 -0800

    solution164 added

---
+++ js/solution164.js
@@ -0,0 +1,5 @@
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 
+// parameters:n, n is the number of customers to buy hotdogs, different numbers 
+// have different prices (refer to the following table), return a number that the 
+// customer need to pay how much money.
+
