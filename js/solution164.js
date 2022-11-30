b4c67ac78a808aef4f2f926cb89433fe19147bbd
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-11-30 14:37:55 -0800

    solution164 completed

--- js/solution164.js
+++ js/solution164.js
@@ -3,3 +3,11 @@
 // have different prices (refer to the following table), return a number that the 
 // customer need to pay how much money.
 
+function saleHotdogs(n){
+    if(n < 5){
+      return n * 100
+    }else if(n >= 5 && n < 10){
+      return n * 95
+    }else
+      return n * 90
+  }
