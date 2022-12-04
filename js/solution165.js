f0cfeef4acab7acaea918880f1bc9d497c039f72
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-03 16:57:30 -0800

    solution166 completed

--- js/solution165.js
+++ js/solution165.js
@@ -10,3 +10,19 @@ function nextPal(val) {
     while ([...String(++val)].reverse().join('') != val);
       return val
 }
+
+// function nextPal(val) {
+//     do {
+//       val++;
+//     } while(!isPalindrome(val))
+//     return val;
+//   }
+  
+//   function isPalindrome(num) {
+//     return num === reverseNum(num);
+//   }
+  
+//   function reverseNum(num) {
+//     return parseInt(num.toString().split('').reverse().join(''));
+//   }
+  
