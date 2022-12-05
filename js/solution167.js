bacd026450e8737b3db028bdd56056c13a3307e1
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-05 14:22:51 -0800

    solution167 completed

--- js/solution167.js
+++ js/solution167.js
@@ -2,3 +2,4 @@
 // for lower case vowels (a, e, i, o, u).
 // If they are, change the array value to a string of that vowel.
 // Return the resulting array.
+isVow = a => a.map(x => /[aeiou]/.test(String.fromCharCode(x)) ? String.fromCharCode(x) : x)
