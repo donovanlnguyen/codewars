3e131af47c9c04e3ab3b2e30622e5e4ef4a6887c
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-06 12:50:57 -0800

    soltuion168 completed

--- js/solution167.js
+++ js/solution167.js
@@ -3,3 +3,5 @@
 // If they are, change the array value to a string of that vowel.
 // Return the resulting array.
 isVow = a => a.map(x => /[aeiou]/.test(String.fromCharCode(x)) ? String.fromCharCode(x) : x)
+
+// const isVow = a => a.map(x=>'aeiou'.includes(y=String.fromCharCode(x)) ? y : x)
