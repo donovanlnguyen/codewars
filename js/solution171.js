562530e4d9efd7953d4545979042252d8cbdbb41
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-13 14:39:45 -0800

    solution171 completed

--- js/solution171.js
+++ js/solution171.js
@@ -6,3 +6,6 @@
 // // As a good parent, you will sit and count with him. Given the number (n), 
 // populate an array with all numbers up to and including that number, 
 // but excluding zero.
+function monkeyCount(n) {
+    return Array.from({length: n},(_,index) => index +1)
+  }
