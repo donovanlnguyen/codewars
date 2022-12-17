ede6b12d0317ab1cdc5c11e99e04479451905e22
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-17 13:21:17 -0800

    solution173 alternative

--- js/solution173.js
+++ js/solution173.js
@@ -36,4 +36,10 @@ const humanYearsCatYearsDogYears = (humanYears) => {
   }
   return [humanYears,catYears,dogYears];
 }
+
+// var humanYearsCatYearsDogYears = function(y) {
+//   if (y == 1) return [1, 15, 15]
+//   if (y == 2) return [2, 24, 24]
+//   return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+// }
   
