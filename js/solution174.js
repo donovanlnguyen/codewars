f2e25ca278084e94a7a42c9b2491ba397a20f606
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-18 13:53:25 -0800

    solution174 added

---
+++ js/solution174.js
@@ -0,0 +1,13 @@
+// You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+
+// Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+
+// Add the method
+
+// isWorthIt
+// to decide if the ship is worthy to loot. For example:
+
+// titanic.isWorthIt() // return false
+// This Kata teaches you the very basics of method creation.
+
+// Good luck!
