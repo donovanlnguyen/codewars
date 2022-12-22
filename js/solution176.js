0874f9116b44805e72fc695141fe71e736503c92
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-22 10:50:34 -0800

    solution176 completed

---
+++ js/solution176.js
@@ -0,0 +1,28 @@
+// Your task is to make a function hotpo that takes a positive n as input and returns the number of times you need to perform this algorithm to get n = 1.
+
+// #Examples
+
+// hotpo(1) returns 0
+// (1 is already 1)
+
+// hotpo(5) returns 5
+// 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+// hotpo(6) returns 8
+// 6 -> 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
+
+// hotpo(23) returns 15
+// 23 -> 70 -> 35 -> 106 -> 53 -> 160 -> 80 -> 40 -> 20 -> 10 -> 5 -> 16 ->
+
+var hotpo = function(n){
+    let count = 0
+    while (n > 1){
+      if(n %2){
+        n = 3 * n + 1
+      }else {
+        n/=2
+      }
+        count++
+      }
+  return count
+}
