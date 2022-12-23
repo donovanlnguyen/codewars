0aa55b5bd848a659d4c7bf8753d7d9b450799d38
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-23 10:52:21 -0800

    solution176 alternative solution

--- js/solution176.js
+++ js/solution176.js
@@ -26,3 +26,11 @@ var hotpo = function(n){
       }
   return count
 }
+
+// var hotpo = function(n, acc = 0) {
+//     if (n <= 1) {
+//       return acc;
+//     } else {
+//       return hotpo(n%2==0 ? n/2 : 3*n+1, acc+1);
+//     }
+//   }
