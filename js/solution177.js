4383c0fc002e6a4ed818b107957987dac6408323
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2022-12-26 11:07:59 -0800

    solution 177 completed

--- js/solution177.js
+++ js/solution177.js
@@ -1,2 +1,3 @@
 // Your task is to write a function that takes a String and 
 // returns an Array/list with the length of each word added to each element.
+const addLength = (str) => str.split(' ').map((e) => `${e} ${e.length}`);
