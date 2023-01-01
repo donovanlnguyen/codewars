6ab69a30dfd7cb4b9041a48377d14edce8df6b1b
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-01 12:41:33 -0800

    solution179 completed

--- js/solution179.js
+++ js/solution179.js
@@ -1,2 +1,11 @@
 // Return a new array consisting of elements 
 // which are multiple of their own index in input array (length > 1).
+function multipleOfIndex(array) {
+    let result = []
+    for(i = 0; i < array.length; i++){
+      if(array[i] % i === 0){
+        result.push(array[i])
+      }
+    }
+    return result
+  }
