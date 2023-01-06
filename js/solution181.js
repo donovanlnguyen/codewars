32b5632b946b49f41e94031bd7b5aad60e965e54
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-06 13:02:54 -0800

    solution181 completed

--- js/solution181.js
+++ js/solution181.js
@@ -17,3 +17,16 @@
 
 // Note: newlines should be added between rows, but there should be no trailing newline 
 // at the end. If you're unsure about the format, look at the sample tests.
+function multiTable(number) {
+    return `1 * ${number} = ${1*number}
+  2 * ${number} = ${2*number}
+  3 * ${number} = ${3*number}
+  4 * ${number} = ${4*number}
+  5 * ${number} = ${5*number}
+  6 * ${number} = ${6*number}
+  7 * ${number} = ${7*number}
+  8 * ${number} = ${8*number}
+  9 * ${number} = ${9*number}
+  10 * ${number} = ${10*number}`
+  }
+
