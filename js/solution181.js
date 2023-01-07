fd6ba633cb6388948e24a9c4a2571d4539ad772c
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-07 13:03:32 -0800

    solution181 alt solution

--- js/solution181.js
+++ js/solution181.js
@@ -30,3 +30,12 @@ function multiTable(number) {
   10 * ${number} = ${10*number}`
   }
 
+//   const multiTable = (number) => {
+//     let table = '';
+    
+//     for(let i = 1; i <= 10; i++) {
+//       table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+//     }
+  
+//     return table;
+//   }
