5e303a591d9a7b36d5a910c6003495920ae271ca
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-09 13:13:17 -0800

    solution182 completed

--- js/solution182.js
+++ js/solution182.js
@@ -2,3 +2,6 @@
 // and state to welcome a person. Note that name will be an array consisting of one 
 // or more values that should be joined together with one space between each, and the 
 // length of the name array in test cases will vary.
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+  }
