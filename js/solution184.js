c45c25e8d112fea9faef92b94facae3466997e2d
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-11 12:10:08 -0800

    solution184 completed

---
+++ js/solution184.js
@@ -0,0 +1,9 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+function checkForFactor (base, factor) {
+    if(base % factor === 0){
+      return true
+    }else
+      return false
+  }
