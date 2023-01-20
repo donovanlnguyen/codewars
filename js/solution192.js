3e9ed043035677f0e1fc454ef3ccb2bbd858e6c6
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-20 13:52:00 -0800

    solution192 completed

--- js/solution192.js
+++ js/solution192.js
@@ -2,3 +2,10 @@
 // sums all the cubed values from 1 to n, and returns that sum.
 
 // Assume that the input n will always be a positive integer.
+function sumCubes(n){
+    let sum = 0
+    for(i = 0; i <=n; i++){
+      sum += Math.pow(i, 3)
+    }
+    return sum
+  }
