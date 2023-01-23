77fc29797eb5d798477df824ad568c43db3f4f18
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-23 14:21:01 -0800

    solution193 completed

--- js/solution193.js
+++ js/solution193.js
@@ -4,3 +4,30 @@
 // Numbers with no identical numbers preceding or following it 
 // returns a 1: 2, 4, 9  => 1, 1, 1
 // Sequential groups of identical numbers return their count: 6, 6, 6, 6 => 4
+function setReducer(arr) {
+    while(arr.length-1) {
+      arr = arr.reduce((a,v,i)=>{
+        if(v!=arr[i-1]) a.push(1)
+        else a[a.length-1]++
+        return a
+      }, [])
+    }
+    return arr.pop()
+  }
+
+//   function setReducer(input) {
+//     // code here
+//     if(input.length === 1) return input[0]
+//     let newInput = []
+//     for(let i = 0,count = 1; i < input.length; i++){
+//       if(input[i] === input[i+1]){
+//         count +=1
+//       }else{
+//         newInput.push(count)
+//         count = 1
+//       }
+//     }
+    
+//     console.log(newInput)
+//     return setReducer(newInput)
+//   }
