1a86a32f362ca29e9bdf2572b6a488c8722a2583
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-25 14:05:59 -0800

    solution195 completed

--- js/solution194.js
+++ js/solution194.js
@@ -3,10 +3,31 @@
 // The cat can jump either one or three shelves at a time: from shelf 
 // i to shelf i+1 or i+3
 // Find the minimum number of jumps to go from start to finish
-function solution(start, finish) 
-{
-  if(finish - start < 3){
-    return start  
-  }else
-    return finish - 3
-}
+
+function solution(start, finish) {
+    var diff = finish - start
+    return Math.floor(diff / 3) + (diff % 3)
+  }
+
+//   function solution(start, finish) 
+// {
+//   let stepsToClimb = finish-start
+
+//   let numBigJumps = Math.floor(stepsToClimb/3)
+
+//   let numSmallJumps = stepsToClimb % 3
+
+//   return numBigJumps + numSmallJumps
+// }
+
+// function solution(start, finish) 
+// {
+//   let jumps = 0;
+//   let iFinish = 0;
+//   for (let i = start; i < finish; i+=3) {
+//     jumps ++
+//     iFinish = i;
+//   }
+//   if ((iFinish+3) - finish === 1) {jumps++}
+//   return jumps;
+// }
