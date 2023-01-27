c4cd242bfaff58f38699ec052db547aa2cad5cb3
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-27 14:23:43 -0800

    solution197 added

---
+++ js/solution197.js
@@ -0,0 +1,9 @@
+// Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces).
+
+// The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words, for example:
+
+// dict["P"] == "perfect"
+// Examples
+// "dgm" ==> "disturbing gregarious mustache"
+
+// "lkj" ==> "literal klingon joke"
