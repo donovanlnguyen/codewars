c4e4eade9e3fd777845c4aa63343311baf7085ef
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-01-30 13:29:13 -0800

    solution198 completed

--- js/solution198.js
+++ js/solution198.js
@@ -2,3 +2,6 @@
 // of the integers inside a string. For example, in the string 
 // "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the 
 // integers is 3635.
+function sumOfIntegersInString(s){
+    return s.replace(/\D/g, ' ').split(' ').reduce((acc, curr) => acc + Number(curr), 0)
+ }
