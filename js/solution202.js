9ce8a2316f4a8dccec70f2e3259a49696c15d34d
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-02-03 18:52:32 -0800

    solution202 added

---
+++ js/solution202.js
@@ -0,0 +1,3 @@
+// For a given list of digits 0 to 9, return a list with the same 
+// digits in the same order, but with all 0s paired. Pairing two 
+// 0s generates one 0 at the location of the first one.
