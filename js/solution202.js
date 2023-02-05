8523a8d1720bdb53ab361d6873d5d4c15d2180f9
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-02-04 19:05:35 -0800

    solution202 completed

--- js/solution202.js
+++ js/solution202.js
@@ -1,3 +1,12 @@
 // For a given list of digits 0 to 9, return a list with the same 
 // digits in the same order, but with all 0s paired. Pairing two 
 // 0s generates one 0 at the location of the first one.
+
+function pairZeros(arr) {
+    var seenZero = 0;
+    return arr.filter(function(num){
+      return num != 0 || seenZero++ % 2 == 0;
+    });
+  }
+
+//   var pairZeros = (a, f) => a.filter(n => n || (f = !f));
