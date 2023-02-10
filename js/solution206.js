ce96016202071b07b677d8f0b64acbfaaa106d2b
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-02-10 13:40:15 -0800

    solution205 added

---
+++ js/solution206.js
@@ -0,0 +1,8 @@
+// You can paint an asperand by pixels in three steps:
+
+// First you paint the inner square, with a side of k.
+// Then you need to paint one pixel, that's laying diagonally relative to the inner square that you just painted ( the bottom-right corner of the inner square is touching the top-left corner of the pixel ). Let's call it the "bridge".
+// Finally, you will need to paint the outer shape, connected diagonally to the "bridge" ( see the picture for more information ).
+// Here are some examples of this:
+
+// Examples for 0<k<5
