611c59f1e366d2caa72b330e9cc9aa76e9625b1b
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-02-18 12:01:46 -0800

    solution205 completed

--- js/solution208.js
+++ js/solution208.js
@@ -7,5 +7,9 @@
 // characters.
 
 function solve(s){
-    //..
+    let upper = (s.match(/[A-Z]/g) || []).length,
+         lower= (s.match(/[a-z]/g) || []).length,
+        number = (s.match(/[0-9]/g) || []).length,
+        special = s.length - upper - lower - number
+    return[upper, lower, number, special]
    }
