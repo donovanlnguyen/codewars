b067a7cafa686bc13a33460e7f6b284797d60dfa
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-02-19 12:03:53 -0800

    solution208 alt added

--- js/solution208.js
+++ js/solution208.js
@@ -13,3 +13,11 @@ function solve(s){
         special = s.length - upper - lower - number
     return[upper, lower, number, special]
    }
+
+//    const solve = x => {
+//     let u = x.match(/[A-Z]/g)||[]
+//     let d = x.match(/[a-z]/g)||[]
+//     let n = x.match(/[0-9]/g)||[]
+//     let s = x.match(/[^A-Z0-9]/gi)||[]
+//     return [u.length, d.length, n.length, s.length]
+//   }
