d10a2585f8e79b3ea9211d63c5c10b9420249389
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-02-21 12:29:57 -0800

    solution209 completed

--- js/solution209.js
+++ js/solution209.js
@@ -1,2 +1,5 @@
 // You'll be given a string, and have to return the sum of all characters as an int. 
 // The function should be able to handle all printable ASCII characters.
+function uniTotal (string) {
+    return string.split('').reduce((prev, curr) => prev + curr.charCodeAt(), 0)
+  }
