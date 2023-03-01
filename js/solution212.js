4b1533e3737f38ea5da51535ab621708e1c9feac
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-01 14:47:17 -0800

    solution212 completed

--- js/solution212.js
+++ js/solution212.js
@@ -6,3 +6,10 @@
 // mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
 // mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
 // mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+
+function mango(quantity, price){
+    if(quantity < 3){
+      return quantity * price
+    }else
+      return (quantity * price) - (price * (Math.floor(quantity/3)))
+  }
