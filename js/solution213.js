e65e6116a0df650d8f404f5f7dfe55fe92fcbd08
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-05 17:15:24 -0800

    solution213 alt solution

--- js/solution213.js
+++ js/solution213.js
@@ -32,3 +32,18 @@ class Person {
   Person.greetExtraTerrestrials = function(raceName){
         return `Welcome to Planet Earth ${raceName}`;
   };
+
+//   class Person {
+//     constructor(firstName,lastName,age,gender){
+//       this.firstName = firstName || "John";
+//       this.lastName = lastName || "Doe";
+//       this.age = age || 0;
+//       this.gender = gender || "Male";
+//     }
+//     sayFullName(){
+//       return `${ this.firstName } ${ this.lastName }`;
+//     }
+//     static greetExtraTerrestrials(raceName){
+//       return `Welcome to Planet Earth ${ raceName }`;
+//     }
+//   }
