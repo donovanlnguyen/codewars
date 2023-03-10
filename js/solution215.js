4c81016eb877de09f0af4e911b739b4ba3a06ce4
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-09 16:44:38 -0800

    solution215 completed

--- js/solution215.js
+++ js/solution215.js
@@ -17,3 +17,13 @@
 // X can be placed before L (50) and C (100) to make 40 and 90. 
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
+
+var romanToInt = function(s) {
+    const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+    let res = 0;
+    s.split('').forEach((num, i) => {
+        if(map[num] < map[s[i + 1]]) res -= map[num];
+        else res += map[num];
+    });
+    return res
+};
