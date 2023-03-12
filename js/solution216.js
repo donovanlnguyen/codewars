5347a1d557b6415033724265a29bc8069d4c49a4
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-11 17:43:05 -0800

    solution216 completed

--- js/solution216.js
+++ js/solution216.js
@@ -2,3 +2,13 @@
 // Write a function to find the longest common prefix string amongst an array of strings.
 
 // If there is no common prefix, return an empty string "".
+var longestCommonPrefix = function(strs) {
+    let prefix = strs.reduce((acc, str) => str.length < acc.length ? str : acc);
+  
+  for (let str of strs) {
+      while (str.slice(0, prefix.length) != prefix) {
+          prefix = prefix.slice(0, -1);
+      }
+  }
+  return prefix;
+};
