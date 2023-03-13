b9cd10692400c34419a3a0ca82d18c9ba3ceabd3
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-12 17:45:31 -0700

    solution216 alt solution

--- js/solution216.js
+++ js/solution216.js
@@ -12,3 +12,16 @@ var longestCommonPrefix = function(strs) {
   }
   return prefix;
 };
+
+// var longestCommonPrefix = function(arr) {
+//     let i = 1;
+//     let prf = arr[0];
+//     while(i < arr.length){
+//         if(!arr[i].startsWith(prf)){
+//             prf = prf.slice(0, -1)
+//         }else{
+//             i++
+//         }
+//     }
+//     return prf
+// };
