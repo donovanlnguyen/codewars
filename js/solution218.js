9e9b2a2af1d14a5df48dd8ea306f420fe61405e1
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-16 10:20:48 -0700

    solution218 completed

--- js/solution218.js
+++ js/solution218.js
@@ -1,3 +1,4 @@
+// 20. Valid Parentheses
 // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 // An input string is valid if:
@@ -5,3 +6,24 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
+var isValid = function(s) {
+    const leftSymbols= []
+    for(let i =0; i < s.length; i++){
+        if(s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            leftSymbols.push(s[i]);
+        }
+     else if(s[i] === ')' && leftSymbols.length != 0 && leftSymbols[leftSymbols.length -1] === '('){
+         leftSymbols.pop();
+     }
+     else if(s[i] === '}' && leftSymbols.length != 0 && leftSymbols[leftSymbols.length -1] === '{'){
+         leftSymbols.pop();
+     }
+     else if(s[i] === ']' && leftSymbols.length != 0 && leftSymbols[leftSymbols.length -1] === '['){
+         leftSymbols.pop();
+     }
+     else{
+         return false;
+     }
+    }
+    return leftSymbols.length === 0;
+};
