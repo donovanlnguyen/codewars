907933e453b76339c96a6a0a30a2f45931ffc082
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-18 13:23:33 -0700

    solution219 completed

--- js/solution219.js
+++ js/solution219.js
@@ -2,3 +2,17 @@
 // return the index if the target is found. If not, return the index where 
 // it would be if it were inserted in order.
 // You must write an algorithm with O(log n) runtime complexity.
+var searchInsert = function(nums, target) {
+    let left = 0, right = nums.length
+   while(left < right) {
+       const mid = left + Math.floor((right - left) / 2)
+       if(nums[mid]===target) {
+          return mid
+       } else if(nums[mid] > target){
+           right = mid
+       } else {
+           left = mid + 1
+       }
+   }
+   return left
+};
