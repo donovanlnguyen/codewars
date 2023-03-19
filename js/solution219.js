3d6f3e986d16c72203b277872f0eb0b0fefadccf
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-19 13:27:13 -0700

    solution219 alt solution

--- js/solution219.js
+++ js/solution219.js
@@ -16,3 +16,23 @@ var searchInsert = function(nums, target) {
    }
    return left
 };
+
+// var searchInsert = function(nums, target) {
+//     let start = 0;
+//     let end = nums.length-1;
+//     if (target < nums[0]) {
+//         return 0
+//     } else if (target > nums[end]) {
+//         return end + 1
+//     }
+//     while(start<= end){
+//         let mid = Math.floor((start+end)/2)
+//         if(target < nums[mid]){
+//             end = mid-1
+//         }else if(target>nums[mid]){
+//             start = mid + 1;
+//         }else{
+//             return mid
+//         }
+//     }
+//     return start
