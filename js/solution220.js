d2323de974f0c14b6766aca6f2a7919ab68fd1ff
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-21 18:08:19 -0700

    solution220 completed

--- js/solution220.js
+++ js/solution220.js
@@ -1,3 +1,15 @@
 // You are climbing a staircase. It takes n steps to reach the top.
 
 // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+var climbStairs = function(n) {
+    let dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(let i =3; i<= n; i++){
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+
+};
