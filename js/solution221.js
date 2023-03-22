092726d78b0a14b2f3a12fc7d28a625c611e1b9c
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-22 16:25:10 -0700

    solution221 completed

--- js/solution221.js
+++ js/solution221.js
@@ -1,1 +1,15 @@
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+var inorderTraversal = function(root) {
+    var res = [];
+ helper(root, res);
+ return res;
+};
+
+var helper = function (root, res) {
+ if (!root) return;
+ if (root.left) helper(root.left, res);
+ res.push(root.val);
+ if (root.right) helper(root.right, res);
+
+};
