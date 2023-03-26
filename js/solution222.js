431823dd078fed609431f4519bca6eca6cdd41ac
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-26 14:31:16 -0700

    solution222 completed

--- js/solution222.js
+++ js/solution222.js
@@ -1,7 +1,14 @@
 // Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 var isSymmetric = function(root) {
-    
-};
+    if (!root) return true;
+    return helper(root.left, root.right);
+  };
+  
+  var helper = function (p, q) {
+    if ((!p && q) || (p && !q) || (p && q && p.val !== q.val)) return false;
+    if (p && q) return helper(p.left, q.right) && helper(p.right, q.left);
+    return true;
+  };
  
 
 
