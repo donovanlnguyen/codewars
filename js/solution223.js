5b38c35a3ba737558718e25119956c8de238cd22
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-03-29 14:18:45 -0700

    solution223 completed

--- js/solution223.js
+++ js/solution223.js
@@ -1,3 +1,8 @@
 // Given the root of a binary tree, return its maximum depth.
 
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+var maxDepth = function(root) {
+    if(!root) return 0;
+    return 1 + Math.max(maxDepth(root.right), maxDepth(root.left))
+};
