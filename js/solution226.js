b9918f12db8c36657c7a1d88d004e563bc945c8b
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-03 15:35:43 -0700

    solution226 completed

--- js/solution226.js
+++ js/solution226.js
@@ -2,3 +2,25 @@
 
 // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
+var generate = function(numRows) {
+    if(numRows < 1) return [];
+    if(numRows === 1) return [[1]];
+
+    const triangle = [[1]];
+
+    for(let i = 1; i < numRows; i++) {
+        let prevRow = triangle[i - 1];
+        const curRow = [];
+    
+
+    curRow.push(1);
+
+        for(let j = 1; j < prevRow.length; j++)     {
+        curRow[j] = prevRow[j] + prevRow[j -1];
+    }
+    curRow.push(1);
+
+    triangle.push(curRow);
+    }
+    return triangle
+};
