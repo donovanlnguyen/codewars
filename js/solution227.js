11520af6aef1766e386721dc8c80788264ed28ee
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-06 15:12:31 -0700

    solution227 alt solution

--- js/solution227.js
+++ js/solution227.js
@@ -16,3 +16,17 @@ var maxProfit = function(prices) {
     }
     return maxProfit
 };
+
+// var maxProfit = function(prices) {
+//     let maxProfit = 0
+//     let minPrice = prices[0]
+
+//     for(let sell = 1; sell < prices.length; sell++){
+//         let sellPrice = prices[sell]
+//         let profit = sellPrice - minPrice
+
+//         maxProfit = Math.max(maxProfit, profit)
+//         if(sellPrice < minPrice) minPrice = sellPrice
+//     }
+//     return maxProfit
+// };
