d5d7a00d10eec68b4b7e6c1bfb47d701691018f5
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-08 15:57:52 -0700

    solution228 alt solution

--- js/solution228.js
+++ js/solution228.js
@@ -10,3 +10,14 @@ return nums[i];
 }
 }
 };
+
+// var singleNumber = function(nums) {
+//     if(nums.length<0){
+//         return 0
+//     }
+//     let num = 0 ;
+//     for (let i = 0; i<nums.length; i++ ){
+//         num^=nums[i]
+//     }
+//     return num
+// };
