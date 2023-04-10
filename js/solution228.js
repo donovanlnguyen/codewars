13de0902241ccf5f42de2a4ccfdb0bca9675e6ec
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-10 15:55:31 -0700

    solution228 completed

--- js/solution228.js
+++ js/solution228.js
@@ -2,3 +2,11 @@
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
+var singleNumber = function(nums) {
+    nums.sort();
+for (var i = 0; i < nums.length; i += 2) {
+if (nums[i] != nums[i + 1]) {
+return nums[i];
+}
+}
+};
