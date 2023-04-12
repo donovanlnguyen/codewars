eb79347a5beb1196c17a9ed292c897219e72ef99
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-12 13:36:51 -0700

    solution229 alt solution

--- js/solution229.js
+++ js/solution229.js
@@ -23,3 +23,13 @@ var hasCycle = function(head) {
    }
    return false;
 };
+
+// const hasCycle = (head) => {
+//     let fast = head;
+//     while (fast && fast.next) {
+//       head = head.next;
+//       fast = fast.next.next;
+//       if (head === fast) return true;
+//     }
+//     return false;
+//   };
