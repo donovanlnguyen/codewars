0d56b478d49c6887df2594479917384505f3740f
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-14 17:10:32 -0700

    solution230 completed

--- js/solution230.js
+++ js/solution230.js
@@ -2,3 +2,31 @@
 // Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 
 // For example, the following two linked lists begin to intersect at node c1:
+
+var getIntersectionNode = function(headA, headB) {
+    var lenA = getLen(headA);
+    var lenB = getLen(headB);
+    let diff = Math.abs(lenA - lenB);
+  
+    if (lenA > lenB) {
+      while (diff--) headA = headA.next;
+    } else {
+      while (diff--) headB = headB.next;
+    }
+  
+    while (headA !== headB) {
+      headA = headA.next;
+      headB = headB.next;
+    }
+  
+    return headA;
+  };
+  
+  var getLen = function (head) {
+    var len = 0;
+    while (head) {
+      len++;
+      head = head.next;
+    }
+    return len;
+  };
