31fef6e476c574eca18d444f0eaaa934f7f2c4ed
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-17 15:41:22 -0700

    solution231 completed

--- js/solution231.js
+++ js/solution231.js
@@ -1,2 +1,6 @@
 // Remove words from the sentence if they contain exactly one exclamation mark. 
 // Words are separated by a single space, without leading/trailing spaces.
+
+function remove(s){
+    return s.split(' ').filter(i => i.split('!').length != 2).join(' ');
+  }
