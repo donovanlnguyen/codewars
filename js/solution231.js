f4640728ff715cb666826a3b5c342ba0dfd57644
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-18 15:42:37 -0700

    solution231 alt solution

--- js/solution231.js
+++ js/solution231.js
@@ -4,3 +4,9 @@
 function remove(s){
     return s.split(' ').filter(i => i.split('!').length != 2).join(' ');
   }
+
+//   const remove = str =>
+//   str
+//   	.split(' ')
+//     .filter(w => w.split('!').length != 2)
+//     .join(' ');
