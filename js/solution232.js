69a0d26fb5a0dcc84ff2e09228b943e8c271a991
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-19 13:54:14 -0700

    solution232 added

---
+++ js/solution232.js
@@ -0,0 +1,6 @@
+// Write a generic function chainer that takes a starting value, 
+// and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function 
+// (except the first function, which takes the starting value as its input). 
+// Return the final value after execution is complete.
