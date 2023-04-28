b8fbf63b9f69dec96e9b4c6f62141ff2b63638fa
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-28 13:59:56 -0700

    solution235 added

---
+++ js/solution235.js
@@ -0,0 +1,7 @@
+// This is a spin off of my first kata.
+
+// You are given a string containing a sequence of character sequences separated by commas.
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
