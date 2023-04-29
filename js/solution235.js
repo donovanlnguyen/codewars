7d7c42bd68e6dd33af1e3da3a3e4653f9a8f83b7
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-04-29 14:03:43 -0700

    solution235 completed

--- js/solution235.js
+++ js/solution235.js
@@ -5,3 +5,7 @@
 // Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
 
 // If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+function array(string) {
+    return string.split(',').slice(1,-1).join(' ') || null
+  }
