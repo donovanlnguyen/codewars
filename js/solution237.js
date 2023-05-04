79a478a980df74c0a4eef44b2c9449d376b22960
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-05-04 16:11:21 -0700

    solution237 completed

--- js/solution237.js
+++ js/solution237.js
@@ -16,3 +16,8 @@
 // you can assume the input is a string.
 // to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 // function should be case insensitive to pass the tests
+
+function validateHello(greetings) {
+    res = /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings)
+    return res
+  }
