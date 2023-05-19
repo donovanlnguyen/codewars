f1664ab3f98a2f4f41a973ab177561896f1935b3
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-05-18 17:29:48 -0700

    solution242 added

--- js/solution241.js
+++ js/solution241.js
@@ -3,9 +3,9 @@
 // For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
 
 function mostLikely(prob1,prob2){
-    let p1 = prob1.split(':');
-    let p2 = prob1.split(':');
-    let sum1 = p1[0] / p1[1];
-    let sum2 = p2[0] / p2[1];
-    return sum2 > sum1
+    let p1=prob1.split(':');
+    let sum1=p1[0]/p1[1];
+    let p2=prob2.split(':');
+    let sum2=p2[0]/p2[1];
+    return sum1>sum2;
   }
