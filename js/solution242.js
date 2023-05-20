1c4e8fc6fefa35b8d065c7f1699e036843e2bff6
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-05-19 17:42:34 -0700

    solution242 completed

--- js/solution242.js
+++ js/solution242.js
@@ -1,2 +1,12 @@
 // Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.
 // For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
+
+function spacey(array){
+    let result = [];
+    let temp = array[0];
+    for(let i=1; i < array.length +1; i++){
+      result.push(temp)
+      temp = temp + array[i];
+    }
+      return result;
+  }
