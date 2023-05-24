3bbcf3ac60806ec6dfa90fed7960d3c89d4dca1a
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-05-24 15:00:40 -0700

    solution245 completed

---
+++ js/solution245.js
@@ -0,0 +1,16 @@
+// // Given an integer n and two other values, build an array of size n filled with these two values alternating.
+// 5, true, false     -->  [true, false, true, false, true]
+// 10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+// 0, "one", "two"    -->  []
+// Good luck!
+function alternate(n, firstValue, secondValue){
+    let result = []
+    
+    for(let i = 0; i < n; i++){
+      if(!(i % 2)){
+        result.push(firstValue)
+      }else
+        result.push(secondValue)
+    }
+    return result
+  }
