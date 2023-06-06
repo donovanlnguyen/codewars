6ddfcdd518abf0f4904abf214b42e5578d4e040e
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-06-06 13:56:03 -0700

    solution249 alt solution

--- js/solution249.js
+++ js/solution249.js
@@ -42,3 +42,12 @@ function stringsConstruction(A, B) {
       }
     return count
   }
+
+//   function stringsConstruction(str, target) {
+//     for(let i = 0;; i++) {
+//       for (let c of str) {
+//         if (target.includes(c)) target = target.replace(c, '-')
+//         else return i;
+//       }
+//     }
+//   }
