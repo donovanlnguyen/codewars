9b850c3230c37390ff6c66a9d8894a4722cf58af
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-06-26 11:36:35 -0700

    solution259 alt solution

--- js/solution259.js
+++ js/solution259.js
@@ -19,3 +19,9 @@ function dominator(arr) {
    return -1;
  }
  
+//  function dominator(arr) {
+//     for(let i=0, obj={}; i<arr.length; i++){
+//         obj[arr[i]]=obj[arr[i]]+1||1
+//         if(obj[arr[i]]>arr.length/2) return arr[i]
+//     }return -1 
+//   }
