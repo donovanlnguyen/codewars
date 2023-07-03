f7ca2378fc69e3458d185ef249e79b1cd4b09b1b
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-07-03 14:56:39 -0700

    solution262 alt solution

--- js/solution262.js
+++ js/solution262.js
@@ -11,3 +11,10 @@
 function isLeapYear(year) {
     return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
   }
+
+//   function isLeapYear(year) {
+//     if(0 == year%400) return true;
+//     if(0 == year%100) return false;
+//     if(0 == year%4) return true;
+//     return false;
+//   }
