1aa6ee13eb91f281ce2a88a034e84b2a8a9b61b4
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-07-10 10:52:02 -0700

    solution264 alt solution

--- js/solution264.js
+++ js/solution264.js
@@ -26,4 +26,16 @@ function minimumPerimeter(area) {
       }
     }
   }
+
+//   function minimumPerimeter(area) {
+//     let w = Math.round(Math.sqrt(area))
+//     let h = (area/w)
+   
+//     while(Number.isInteger(h) === false){
+//       w = w + 1
+//       h = (area/w)
+//     }
+  
+//     return w+w+h+h
+//   }
   
