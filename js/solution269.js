1ff8919ea464868a18ea074272ce25833b23699c
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-07-20 15:33:45 -0700

    solution269 completed

--- js/solution269.js
+++ js/solution269.js
@@ -14,3 +14,15 @@
 // There should be one space in between the two characters
 // You don't have to modify the case (i.e. no need to change to upper or lower)
 // If one string is longer than the other, there should be a space where the character would be
+
+function transposeTwoStrings (array) {
+	var result = [];
+  var len = Math.max(array[0].length, array[1].length);
+  
+  for (var i = 0; i < len; i++) {
+    var column = (array[0][i] || " ") + " " + (array[1][i] || " ");
+    result.push(column);
+  }
+  
+  return result.join("\n");
+}
