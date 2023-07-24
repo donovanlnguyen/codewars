0b7d7e622b40dabf0015746981eec6039a10b6e2
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-07-24 14:28:33 -0700

    solution270 complete

--- js/solution270.js
+++ js/solution270.js
@@ -14,5 +14,8 @@
 // center('abc', 10, '_')  # returns "____abc___"
 // center('abcdefg', 2)  # returns "abcdefg"
 function center (strng, width, fill = ' ') {
-    return '';
+    if (strng.length >= width) return strng;
+    let r = fill.repeat(Math.floor((width - strng.length) / 2));
+    let l = fill.repeat(Math.ceil((width - strng.length) / 2));
+    return l + strng + r;
   }
