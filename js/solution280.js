93e3a6460ee2382ec67bb031396eaf8b4bbf05e9
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-08-17 15:39:47 -0700

    solution280 added

---
+++ js/solution280.js
@@ -0,0 +1,6 @@
+// You'll be given a list of two strings, and each will contain exactly one colon (":") 
+// in the middle (but not at beginning or end). The length of the strings, before and after the colon, 
+// are random.
+
+// Your job is to return a list of two strings (in the same order as the original list), 
+// but with the characters after each colon swapped.
