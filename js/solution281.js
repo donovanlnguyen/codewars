30a0ba885d6774671b1ea7412c0672ee4b76fe54
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-08-21 13:25:27 -0700

    solution281 completed

--- js/solution281.js
+++ js/solution281.js
@@ -11,3 +11,12 @@
 //   18876  =>  should return 'Saturday 02:36'
 //  259180  =>  should return 'Thursday 23:40' 
 // -349000  =>  should return 'Tuesday 15:20'
+
+const dayAndTime = minutes => {
+    var date = new Date(0, 0, 0),
+      daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    date.setTime(date.getTime() + minutes * 60 * 1000);
+    return `${daysOfWeek[date.getDay()]} ${formatNumber(date.getHours())}:${formatNumber(date.getMinutes())}`;
+}
+
+const formatNumber = number => (number < 10 ? "0" : "") + number;
