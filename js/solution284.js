a0e95981490db1354a5906f7314ef942060f2855
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-08-27 19:02:50 -0700

    solution284 added

---
+++ js/solution284.js
@@ -0,0 +1,2 @@
+// Write a function that takes a string of parentheses, and determines if the order of the parentheses 
+// is valid. The function should return true if the string is valid, and false if it's invalid.
