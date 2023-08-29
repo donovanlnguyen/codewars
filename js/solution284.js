5b106d19d7d9a2106060057a1d0859e43ce7e62e
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-08-28 21:19:49 -0700

    solution284 completed

--- js/solution284.js
+++ js/solution284.js
@@ -1,2 +1,16 @@
 // Write a function that takes a string of parentheses, and determines if the order of the parentheses 
 // is valid. The function should return true if the string is valid, and false if it's invalid.
+function validParentheses(s) {
+    const hashMap = {"(": ")", "{": "}", "[": "]"}
+    const stack = []
+    for(let ch of s){
+      if(hashMap[ch]){
+        stack.push(hashMap[ch])
+      }else if(stack.length > 0 && stack[stack.length -1] === ch) {
+        stack.pop()
+      }else{
+        return false
+      }
+    }
+    return stack.length === 0 
+  }
