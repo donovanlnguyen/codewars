f65cf379f50260392509d20c923a27552caee53a
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-08-31 14:05:38 -0700

    solution286 added

---
+++ js/solution286.js
@@ -0,0 +1,6 @@
+// Define a function that will receive a logarithm function, and returns the base of that logarithm.
+
+// guessBase(ln) == e
+// Base is a real number (not only integers) guaranteed to be less than 1e6.
+
+// Have a fun time coding!
