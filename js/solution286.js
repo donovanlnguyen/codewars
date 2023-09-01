14b94ab19c7e19d1c19bdaa303725cd33001097d
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-09-01 14:14:59 -0700

    solution286 completed

--- js/solution286.js
+++ js/solution286.js
@@ -4,3 +4,9 @@
 // Base is a real number (not only integers) guaranteed to be less than 1e6.
 
 // Have a fun time coding!
+
+determineBase=f=>2**f(2)**-1
+
+function determineBase(logFunc) {
+    return Math.exp(Math.log(100) / logFunc(100));
+}
