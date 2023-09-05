20f5112bfd14293108e388e5c0e637405a3d9900
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-09-04 21:50:04 -0700

    solution287 completed

--- js/solution287.js
+++ js/solution287.js
@@ -14,3 +14,9 @@
 // 123
 // 123"
 // Note: 9 digits so 3 squared (3x3 perfect square). 3 digits on each line.
+
+function squareIt(int) {
+    const str = int.toString();
+      const r = Math.sqrt(str.length);
+    return Number.isInteger(r) ? str.replace(RegExp(`(.{${r}})(?!$)`, "g"), "$1\n") : "Not a perfect square!";
+  }
