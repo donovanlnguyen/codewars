38218b4c3b8b39cb668159dc57e9be33355fdc2f
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-09-28 15:04:45 -0700

    solution 293 alt

--- js/solution293.js
+++ js/solution293.js
@@ -20,3 +20,11 @@ function noIfsNoButs(a, b) {
     }
     return a + " is equal to " + b
   }
+
+//   var noIfsNoButs = function (a,b) {
+//     switch (Math.sign(a-b)) {
+//       case 1: return `${a} is greater than ${b}`;
+//       case -1:return `${a} is smaller than ${b}`;
+//       case 0: return `${a} is equal to ${b}`;
+//     }
+//   }
