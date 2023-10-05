cfba2404fab1b0588545a6adf13641532a47dd50
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2023-10-05 15:33:07 -0700

    solution294 alt solution

--- js/solution294.js
+++ js/solution294.js
@@ -22,3 +22,9 @@ function scoreboard(string) {
     string.split` `.map(v=>arr.push(dict[v]))
     return arr.filter(v=>v!==undefined)
   }
+
+//   function scoreboard(string) {
+//     var arr=string.split(' ');
+//     var a=['nil','one','two','three','four','five','six','seven','eight','nine']
+//     return [a.indexOf(arr[arr.length-2]),a.indexOf(arr[arr.length-1])];
+//   }
