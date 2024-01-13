540030b38f69c6d6c1052d12911b91b1638c1886
Donovan Nguyen <95204176+donovanlnguyen@users.noreply.github.com>
2024-01-12 16:38:38 -0800

    solution296 completed

---
+++ js/solution296.js
@@ -0,0 +1,15 @@
+// Arthur needs you to return true if he needs to invite more women or false if he is all set.
+
+// Input/Output
+// [input] integer array L ($a in PHP)
+// An array (guaranteed non-associative in PHP) representing the genders of the attendees, where -1 represents women and 1 represents men.
+
+// 2 <= L.length <= 50
+
+// [output] a boolean value
+
+// true if Arthur need to invite more women, false otherwise.
+
+function inviteMoreWomen(L) {
+    return L.reduce((a,b)=>a+b,0)>0
+    }
